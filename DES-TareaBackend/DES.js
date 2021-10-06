e50f52751c9d4c88afebc09900ee3304491734a5
luisferdev11 <lrodriguezd1900@alumno.ipn.mx>
2021-10-06 17:49:14 -0500

    Avances suficientes para que empiece a trabajar el frontend

--- DES-TareaBackend/DES.js
+++ DES-TareaBackend/DES.js
@@ -1,34 +1,45 @@
 const crypto = require('crypto');
-const fs = require('');
+const fs = require('fs');
 
 const config = {
     cryptkey: "5TGB&YHN7UJM(IK<5TGB&YHN",//24
     iv: "!QAZ2WSX"//8
 };
 
+const text = fs.readFileSync('./files/texto.txt', 'utf8');
+
+
 function encryptText(text){
     //console.log(config.cryptkey);
     const cipher = crypto.createCipheriv('des-ede3-cbc', config.cryptkey, config.iv);
-    let crypted = cipher.update(text,'utf8','binary');
-    crypted += cipher.final('binary');
-    crypted = new Buffer.from(crypted, 'binary').toString('base64');
-    return crypted;
+
+    const input = fs.createReadStream('./files/texto.txt');
+    const output = fs.createWriteStream('./files/texto.txt.cifrado');
+
+    input.pipe(cipher).pipe(output);
 }
 
 function decryptText(text){
     // que sea texto válido
     if (text === null || typeof text === 'undefined' || text === '') {return text;};
-    text = new Buffer.from(text, 'base64').toString('binary');
+    text = new Buffer.from(text, 'base64');
     const decipher = crypto.createDecipheriv('des-ede3-cbc', config.cryptkey, config.iv);
     let dec = decipher.update(text,'binary','utf8');
     dec += decipher.final('utf8');
     return dec;
 }
 
-const textOriginal = "Here is some data to encrypt!";
 
-a = encryptText(textOriginal);
-console.log("Enc: " + a);
+encryptText(text);
 
-b = decryptText(a);
-console.log("Dec: " + b);
+
+// fs.createReadStream('./files/texto.txt').pipe(fs.createWriteStream('./files/texto.txt.cifrado'));   //encryptText(text);
+
+//encriptamos
+// const encriptado = encryptText(text);
+// console.log("enc: " + encriptado);
+// fs.writeFileSync('./files/texto.txt.cifrado', encriptado);
+//console.log("Enc: " + encriptado);
+
+// const desencriptado = decryptText(encriptado);
+// console.log("Dec: " + desencriptado);
