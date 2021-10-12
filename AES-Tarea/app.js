7aa167b84069eec3d62fbdd1ed3e3a64181d1ee6
luisferdev11 <lrodriguezd1900@alumno.ipn.mx>
2021-10-12 02:05:56 -0500

    AES logica practicamente terminada, falta frontend, validaciones y lo que vaya surgiendo

--- AES-Tarea/app.js
+++ AES-Tarea/app.js
@@ -13,3 +13,31 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.post("/encriptar", (req, res) => {
+    const nombreArchivo = "mensajeEncriptado.txt";
+    const inputContenido = req.files.contenido.data.toString();
+    const inputClave = req.body.Password;
+
+    const encriptado = Buffer.from(
+        CryptoJS.AES.encrypt(inputContenido, inputClave).toString()
+    );
+
+    fs.writeFileSync(`./files/${nombreArchivo}`, encriptado);
+    res.download(`./files/${nombreArchivo}`);
+});
+
+app.post("/desencriptar", function (req, res) {
+    const nombreArchivo = "mensajeDesencriptado.txt";
+    const inputContenido = req.files.contenido.data.toString();
+    const inputClave = req.body.Password;
+
+    const desencriptado = Buffer.from(
+        CryptoJS.AES.decrypt(inputContenido.toString(), inputClave).toString(
+            CryptoJS.enc.Utf8
+        )
+    );
+
+    fs.writeFileSync(`./files/${nombreArchivo}`, desencriptado);
+    res.download(`./files/${nombreArchivo}`);
+});
